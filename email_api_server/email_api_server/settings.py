fe6e8e87c4ecbefb84f4a023d67035f589f51116
Neha Kumari <neharoberts32@gmail.com>
2022-03-22 00:18:12 +0530

    Modified

--- email_api_server/email_api_server/settings.py
+++ email_api_server/email_api_server/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6)z_kms9nr23qsa10yasm4z0qlxo8ib#85^obt_%x!+b%r&2yq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
